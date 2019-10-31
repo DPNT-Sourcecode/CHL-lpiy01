28c51fe9d47c3ac4b2865c3b6967dc163ba2691c
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 20:35:39 +0000

    Thu Oct 31 20:35:39 UTC 2019

--- src/main/java/befaster/solutions/CHL/OfferMultiBuyPrice.java
+++ src/main/java/befaster/solutions/CHL/OfferMultiBuyPrice.java
@@ -1,5 +1,33 @@
 package befaster.solutions.CHL;
 
-public class OfferMultiBuyPrice {
+/**
+ * Class representing a "multiple buy price" offer , for example 5 for 200.
+ */
+public class OfferMultiBuyPrice implements Offer
+{
+	private final int offerQuantity;
+	private final int offerPrice;
+	
+	/**
+	 * Construct a "multiple buy price" offer.
+	 * 
+	 * @param offerQuantity Offer quantity.
+	 * @param offerPrice Offer price.
+	 */
+	public OfferMultiBuyPrice(final int offerQuantity, final int offerPrice)
+	{
+		this.offerQuantity = offerQuantity;
+		this.offerPrice = offerPrice;
+	}
+	
+	public int getOfferQuantity()
+	{
+		return offerQuantity;
+	}
 
+	public int getOfferPrice()
+	{
+		return offerPrice;
+	}
 }
+
