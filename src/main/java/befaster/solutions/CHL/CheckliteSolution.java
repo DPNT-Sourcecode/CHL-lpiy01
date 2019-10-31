c06284db07f7be8a1a9252f6fcbf1694e3c9d47d
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 19:19:14 +0000

    Thu Oct 31 19:19:14 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -2,8 +2,12 @@ package befaster.solutions.CHL;
 
 import befaster.runner.SolutionNotImplementedException;
 
-public class CheckliteSolution {
-    public Integer checklite(String skus) {
-        throw new SolutionNotImplementedException();
+public class CheckliteSolution
+{
+    public Integer checklite(String skus)
+    {
+    	System.out.println(skus);
+    	return 0;
     }
 }
+
