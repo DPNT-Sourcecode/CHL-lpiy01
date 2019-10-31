1eb8b15e13bee5253c833dbe60d2bcccc9174c43
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 19:28:14 +0000

    Thu Oct 31 19:28:14 UTC 2019

--- src/main/java/befaster/solutions/CHL/Product.java
+++ src/main/java/befaster/solutions/CHL/Product.java
@@ -1,5 +1,23 @@
 package befaster.solutions.CHL;
 
-public class Product {
+public class Product
+{
+	private final String sku;
+	private final int price;
+	private int quanityInBasket;
+	
+	/**
+	 * Construct a new Product with a SKU and a price.
+	 * 
+	 * @param sku Product SKU;
+	 * @param price Product price.
+	 */
+	public Product(final String sku, final int price)
+	{
+		this.sku = sku;
+		this.price = price;
+		this.quanityInBasket = 0;
+	}
 
 }
+
