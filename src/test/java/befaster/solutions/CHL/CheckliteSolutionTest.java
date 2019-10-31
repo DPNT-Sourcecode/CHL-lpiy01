8e6fc6824bb605dc75c4e832d1cf9536952806a6
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 21:37:59 +0000

    Thu Oct 31 21:37:59 UTC 2019

---
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -0,0 +1,24 @@
+package befaster.solutions.CHL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckliteSolutionTest
+{
+	private CheckliteSolution sut;
+	
+    @Before
+    public void setUp()
+    {
+    	sut = new CheckliteSolution();
+    }
+
+	@Test
+	public void testAAA()
+	{
+		System.out.println(sut.checklite("AAA"));
+	}
+
+}
