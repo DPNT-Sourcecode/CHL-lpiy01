1f944f5f2bd79e2c92d4279b69287065a1dce7af
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 19:40:14 +0000

    Thu Oct 31 19:40:14 UTC 2019

--- src/main/java/befaster/solutions/CHL/Product.java
+++ src/main/java/befaster/solutions/CHL/Product.java
@@ -3,21 +3,28 @@ package befaster.solutions.CHL;
 public class Product
 {
 	private final String sku;
-	private final int price;
+	private final int standardPrice;
+	private final int offerQuantity;
+	private final int offerPrice;
 	private int quanityInBasket;
 	
 	/**
-	 * Construct a new Product with a SKU and a price.
+	 * Construct a new Product with SKU , standard price , offer quantity and offer price.
 	 * 
 	 * @param sku Product SKU;
-	 * @param price Product price.
+	 * @param standardPrice Standard price.
+	 * @param offerQuantity Offer quantity , or zero if no special offers.
+	 * @param offerPrice Offer price , or zero if no special offers.
 	 */
-	public Product(final String sku, final int price)
+	public Product(final String sku, final int standardPrice, final int offerQuantity, final int offerPrice)
 	{
 		this.sku = sku;
-		this.price = price;
+		this.standardPrice = standardPrice;
+		this.offerQuantity = offerQuantity;
+		this.offerPrice = offerPrice;
 		this.quanityInBasket = 0;
 	}
 
 }
+
 
