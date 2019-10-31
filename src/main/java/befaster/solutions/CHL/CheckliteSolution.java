27c9fcc472d3f1d5bb9301aae2b12d0703167ee4
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 19:37:14 +0000

    Thu Oct 31 19:37:14 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,12 +1,29 @@
 package befaster.solutions.CHL;
 
+import java.util.HashMap;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckliteSolution
 {
+	private HashMap<String, Product> priceTable = buildPriceTable();
+	
+	private HashMap<String, Product> buildPriceTable()
+	{
+		HashMap<String, Product> priceTable = new HashMap<String, Product>();
+		priceTable.put("A", new Product("A", 50));
+		priceTable.put("B", new Product("B", 30));
+		priceTable.put("C", new Product("C", 20));
+		priceTable.put("D", new Product("D", 15));
+		return priceTable;
+	}
+	
+	
+	
     public Integer checklite(String skus)
     {
     	System.out.println(skus);
     	return 0;
     }
 }
+
