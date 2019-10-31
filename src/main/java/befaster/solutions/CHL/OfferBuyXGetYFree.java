db2ad086f42dc964059b3af2d0dbaef309fbbaf0
sbx_user1051 <sbx_user1051@169.254.186.85>
2019-10-31 20:41:39 +0000

    Thu Oct 31 20:41:39 UTC 2019

--- src/main/java/befaster/solutions/CHL/OfferBuyXGetYFree.java
+++ src/main/java/befaster/solutions/CHL/OfferBuyXGetYFree.java
@@ -1,5 +1,33 @@
 package befaster.solutions.CHL;
 
-public class OfferBuyXGetYFree {
+/**
+ * Class representing a "buy X get Y free" offer.
+ */
+public class OfferBuyXGetYFree implements Offer
+{
+	private final int quantityNeeded;
+	private final int quantityFree;
+	
+	/**
+	 * Construct a new "buy X get Y free" offer.
+	 * 
+	 * @param quantityNeeded Quantity which must be purchased before this offer can be applied.
+	 * @param quantityFree Quantity which can be free.
+	 */
+	public OfferBuyXGetYFree(final int quantityNeeded, final int quantityFree)
+	{
+		this.quantityNeeded = quantityNeeded;
+		this.quantityFree = quantityFree;
+	}
 
+	public int getQuantityNeeded()
+	{
+		return quantityNeeded;
+	}
+	
+	public int getQuantityFree()
+	{
+		return quantityFree;
+	}
 }
+
